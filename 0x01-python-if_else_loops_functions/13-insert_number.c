086fb8d1f2999d8ec367cd90b0e537c274ba45d5
Naikumi <naikumimartha@gmail.com>
2023-06-07 02:35:03 -0700

    address of the new node

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * insert_node - inserts a number sorted in a list
+ * @head: data type double pointer of a list
+ * @number: data type integer number to be added
+ * Return: 0 if no cycle, 1 otherwise
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *node = *head;
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+
+	if (node == NULL || node->n >= number)
+	{
+		new->next = node;
+		*head = new;
+		return (new);
+	}
+	while (node && node->next && node->next->n < number)
+		node = node->next;
+
+	new->next = node->next;
+	node->next = new;
+
+	return (new);
+}
