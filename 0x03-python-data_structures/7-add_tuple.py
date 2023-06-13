00981e44bee61cec66d4f0f93bb8c0d465a9f3c7
Naikumi <naikumimartha@gmail.com>
2023-06-12 19:30:48 -0700

    Tuples addition

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_c = (0, 0)
+    if len(tuple_a) < 2:
+        tuple_a = tuple_a + tuple_c
+    if len(tuple_b) < 2:
+        tuple_b = tuple_b + tuple_c
+    val1 = tuple_a[0] + tuple_b[0]
+    val2 = tuple_a[1] + tuple_b[1]
+    tuple_d = (val1, val2)
+    return(tuple_d)
