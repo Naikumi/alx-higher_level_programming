0712cd667b4578012c9ffd4229a3c02b199d57d4
Naikumi <naikumimartha@gmail.com>
2023-06-14 11:37:48 -0700

    magic

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from magic_calculation_102 import add, sub
+
+
+def magic_calculation(a, b):
+    if a < b:
+        c = add(a, b)
+        for j in range(4, 6):
+            c = add(c, j)
+        return c
+
+    else:
+        return sub(a, b)
